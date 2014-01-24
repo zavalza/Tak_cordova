d1d57b99c4c07e89e0fa724891f76c07d1cc8c93
Paul Zavalza <paulz_91@hotmail.com>
2014-01-23 18:54:49 -0600

    Se añanden los archivos iniciales de furatto

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html >
+	<head> 
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="stylepaginapersonal.css" />
+	<title> Tak </title>
+
+	</head>
+	
+	<body>
+
+	<div id="fb-root"></div> <script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-post" data-href="https://www.facebook.com/INCmty/posts/447982928656475" data-width="550"><div class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/INCmty/posts/447982928656475">Post</a> by <a href="https://www.facebook.com/INCmty">INCmty</a>.</div></div>
+
+	</body>
+
+</html>
